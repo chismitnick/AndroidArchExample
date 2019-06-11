f88756c6d55e08c082a05e132c37bd5c1acdd110
csamiselo <chisambwe.samiselo@gmail.com>
2019-06-11 14:15:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidarchexample/Note.java
@@ -0,0 +1,55 @@
+package com.example.androidarchexample;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title ;
+
+    private String description ;
+
+    private int priority;
+
+    // Constructor
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    //Setter Method for Id field
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter Methods
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/androidarchexample/NoteRepository.java
@@ -0,0 +1,111 @@
+package com.example.androidarchexample;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    //Pass the application as an object
+    public NoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao();
+        allNotes = noteDao.getAllNotes();
+    }
+
+    //method to insert notes
+    public void insert(Note note) {
+
+    }
+
+    //method to update notes
+    public void update(Note note) {
+
+    }
+
+    //method to delete notes
+    public void delete(Note note) {
+
+    }
+
+    //method to delete all notes
+    public void deleteAllNotes() {
+
+    }
+
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+
+
+
+    //Asynctask used because android prevents database operations from executing on the main thread
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        public InsertNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        public UpdateNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        public DeleteNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        public DeleteAllNotesAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.DeleteAllNotes();
+            return  null;
+
+        }
+    }
+
+
+}
